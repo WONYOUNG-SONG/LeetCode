c74d5844db4d80dc499142f7854df6d4aa4f0ac0
WONYOUNG-SONG <120218274+WONYOUNG-SONG@users.noreply.github.com>
2023-05-18 09:14:20 -0400

    Time: 64 ms (27.41%) | Memory: 16.3 MB (35.43%) - LeetSync

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,5 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        Roman_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100,"D": 500, "M": 1000}       
+        
+        return sum(map(lambda x: Roman_dic[x], s.replace("IV","IIII").replace("IX","VIIII").replace("XL","XXXX").replace("XC","LXXXX").replace("CD","CCCC").replace("CM","DCCCC")))
